587471e4654b43083a60589577c615fab5749c09
Gabriel J. Michael <gabriel.j.michael@gmail.com>
2015-05-07 14:19:22 -0400

    Added parallelization for XML to JSON convesion and NoSQL insertion; improved Unicode handling

--- put_json.py
+++ put_json.py
@@ -4,35 +4,54 @@ Created on Wed May  6 14:08:53 2015
 
 @author: gjm
 
-Puts JSON documents into the CouchDB
+Puts JSON documents into the CouchDB. Parallelized.
+Takes two command line arguments; first is "doctype" to distinguish different 
+kinds of documents; second is the path to look for JSON files.
+It assumes all the files at that path are of the same doctype.
 """
 
 import sys
 import requests
 import os
 import json
+from multiprocessing import Pool
+from itertools import repeat
 
-couchdb_url = "http://127.0.0.1:5984/games/"
+couchdb_url = "http://127.0.0.1:5984/yabr/"
+num_processes = 4
+
+def post_json(filename, path, doctype):
+    if filename.endswith(".json"):
+        #print(filename)
+        json_file = open(os.path.join(path, filename), 'r', encoding='utf8')
+        # We have to convert the JSON to a Python dict, then index it, 
+        # because the first element is always something like "id" or "thread"
+        # and all other data is nested under it, so we want to discard the first element.
+        obj = json.load(json_file, encoding='utf8')
+        obj_data = obj[list(obj)[0]]
+        # Also we want to set a "doctype" key to indicate what kind of 
+        # document we are loading. This is not in the original data, and 
+        # comes from the first command line argument.
+        obj_data['doctype'] = doctype
+        # Convert back to JSON for POSTing
+        # The implicit default ensure_ascii=True is necessary here;
+        # Otherwise it is technically invalid JSON which CouchDB rejects
+        json_data = json.dumps(obj_data, sort_keys=True, indent=4)
+        headers = {'Content-type': 'application/json;charset=UTF-8'}
+        r = requests.post(couchdb_url, data=json_data, headers=headers)
+        if r.status_code != 201:
+            print("Uh oh! Got status code", r.status_code, "while POSTing", filename)   
 
 def main():
+    doctype = sys.argv[1]
+    insert_path = sys.argv[2]
     
-    insert_path = sys.argv[1]
-    
-    for filename in os.listdir(insert_path):
-        if filename.endswith(".json"):
-            print(filename)
-            json_file = open(os.path.join(insert_path, filename), 'r', encoding='utf8')
-            # We have to convert the JSON to a Python dict, then index it, 
-            # because the first element is always something like "id" or "thread"
-            # and all other data is nested under it, so we want to discard the first element.
-            obj = json.load(json_file, encoding='utf8')
-            obj_data = obj[list(obj)[0]]
-            # Convert back to JSON for POSTing
-            json_data = json.dumps(obj_data, sort_keys=True, indent=4)
-            headers = {'Content-type': 'application/json'}
-            r = requests.post(couchdb_url, data=json_data, headers=headers)
-            if r.status_code != 201:
-                print("Uh oh! Got status code", r.status_code, "while POSTing", filename)
+    file_list = os.listdir(insert_path)
+
+    with Pool(num_processes) as p:
+        p.starmap(post_json, zip(file_list, 
+                                 repeat(insert_path, len(file_list)), 
+                                 repeat(doctype, len(file_list))))
             
 if __name__ == "__main__":
     main()
--- xml_to_json.py
+++ xml_to_json.py
@@ -6,7 +6,9 @@ Created on Wed May  6 11:11:19 2015
 
 Converts XML files retrieved through the BGG API to JSON files
 Handles both individual items and files with many items
-Takes a single argument, which is the path of the XML files to process
+Takes a single argument, which is the path of the XML files to process. 
+
+Parallelized.
 
 If you get weird parse errors, look for leading ... at the beginning of 
 XML files, which oddly appeared in less than a dozen of several hundred for no apparent reason.
@@ -18,34 +20,88 @@ import xmltodict
 import xml.etree.ElementTree as ET
 import json
 import os
+from multiprocessing import Pool
+from itertools import repeat
+import html
+import re
 
-def xml_to_json(root, path, filename):
+num_processes = 4
+
+def to_json(root, path, filename):
     json_filename = filename[:-4].zfill(len(filename[:-4])) + '.json'    
-    print("xmltodict", json_filename)
+    #print("xmltodict", json_filename)   
     root_dict = xmltodict.parse(ET.tostring(root, encoding='unicode'))
+    # Don't use ensure_ascii=True here;
+    # We want the \u escaped strings is the JSON output
     root_json_str = json.dumps(root_dict, sort_keys=True, indent=4)
     json_file = open(os.path.join(path, json_filename), 'w', encoding='utf8')
     json_file.write(root_json_str)
-    print("Wrote", json_filename)
+    #print("Wrote", json_filename)
     json_file.close()
 
-def main():
-    conversion_path = sys.argv[1]
-    
-    for filename in os.listdir(conversion_path):
+def xml_to_json(filename, path):
+    """
+    Get XML from file, pass to to_json.
+    Handles files with multiple items.
+    Also handle parsing errors caused by undefined entities.
+    These are typically valid HTML entities improperly used in XML.
+    Reopens the file, find the entity, convert it to UTF-8, and rewrites the file.
+    """
+    try:
         if filename.endswith(".xml"):
-            print("ElementTree", filename)
-            tree = ET.parse(os.path.join(conversion_path, filename))
+            #print("ElementTree", filename)
+            tree = ET.parse(os.path.join(path, filename))
             root = tree.getroot()
-            
+            # Avoid converting missing threads or other missing responses
+            if root.tag == 'error':
+                return
             # Multi-item files have this root tag
-            if root.tag == 'items':
+            elif root.tag == 'items':
                 for child in root:
                     zfill_len = len(filename.split('-')[0])
                     true_filename = child.get('id').zfill(zfill_len) + '.xml'
-                    xml_to_json(child, conversion_path, true_filename)
+                    to_json(child, path, true_filename)
             else:
-                xml_to_json(root, conversion_path, filename)
+                to_json(root, path, filename)
+    except ET.ParseError as e:
+        if e.code == 11:
+            print(os.path.join(path, filename), e)
+    
+            # Make a backup of the original file
+            filenamepath = os.path.join(path, filename)
+            backup_filenamepath = filenamepath + '.bak'
+            # Delete existing backup file silently, if any
+            try:
+                os.unlink(backup_filenamepath)
+            except OSError:
+                pass
+            os.rename(filenamepath, backup_filenamepath)
+            xml_file = open(backup_filenamepath, 'r')
+            new_xml_file = open(filenamepath, 'w')
+            # Read backup file line by line, replacing entities as necessary
+            # Wrie output to original file name
+            for i, line in enumerate(xml_file):
+                # Find entity using line, column tuple from e.position
+                if i + 1 == e.position[0]:
+                    entity = line[e.position[1]:].split(';')[0] + ';'
+                    # Convert to unicode representation
+                    new_entity = html.unescape(entity)
+                    print(entity, "->", new_entity, file=sys.stderr)
+                    new_xml_file.write((re.sub(entity, new_entity, line)))
+                else:
+                    new_xml_file.write(line)
+            xml_file.close()
+            new_xml_file.close()
+            # Recursively call ourself to handle the next error in the same file
+            xml_to_json(filename, path)
+        else:
+            print("Unhandled ParseError", e, "in", filename)
 
+def main():
+    conversion_path = sys.argv[1]
+    file_list = os.listdir(conversion_path)
+    with Pool(num_processes) as p:
+        p.starmap(xml_to_json, zip(file_list, repeat(conversion_path, len(file_list))))
+    
 if __name__ == "__main__":
     main()
